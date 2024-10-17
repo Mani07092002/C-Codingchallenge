e6644ce736d2bebb18416dc64baccb22438dc0f8
MOGALLAPALLI MANIKANTA <140966613+Mani07092002@users.noreply.github.com>
2024-10-17 18:31:57 +0530

    Add files via upload

---
+++ Loan.sql
@@ -0,0 +1,21 @@
+create database Loan
+use loan
+CREATE TABLE Customer (
+    CustomerId INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL, 
+    EmailAddress VARCHAR(50) NOT NULL,
+    PhoneNumber Numeric(10),
+    Address VARCHAR(50), 
+    CreditScore Numeric(3) NOT NULL
+);
+
+CREATE TABLE Loan (
+    LoanId INT PRIMARY KEY IDENTITY(1,1),
+    CustomerId INT FOREIGN KEY REFERENCES Customer(CustomerId),
+    PrincipalAmount DECIMAL(18, 2) NOT NULL,
+    InterestRate DECIMAL(5, 2) NOT NULL,
+    LoanTerm INT NOT NULL,
+    LoanType VARCHAR(20) NOT NULL CHECK (LoanType IN ('CarLoan', 'HomeLoan')),
+    LoanStatus VARCHAR(10) NOT NULL CHECK (LoanStatus IN ('Pending', 'Approved'))
+);
+select * from Customer
