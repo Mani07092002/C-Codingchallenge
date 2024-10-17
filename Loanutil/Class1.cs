64713a5502a68f000666789189700387c605a947
MOGALLAPALLI MANIKANTA <140966613+Mani07092002@users.noreply.github.com>
2024-10-17 21:20:15 +0530

    Add files via upload

---
+++ Loanutil/Class1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Runtime.ConstrainedExecution;
+namespace Loanutil
+{
+    public class DBUtil
+    {
+        private static IConfigurationRoot configuration;
+        static string s = null;
+        static DBUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("C: \\Users\\ACER\\source\repos\\Loanutil\appsettings.json", optional: true, reloadOnChange: true);
+            configuration = builder.Build();
+
+
+
+        }
+        public static string ReturnCn(string key)
+        {
+            string connectionString = configuration.GetConnectionString(key);
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                return connectionString;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static SqlConnection GetDBConn()
+        {
+            string connectionString = "Data Source=DESKTOP-295VFEG\\SQLEXPRESS;Initial Catalog=Loan;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+            return conn;
+        }
+    }
+}
